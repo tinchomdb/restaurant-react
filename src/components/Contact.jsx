1f2e97ef01ae9514b04bfbe2a579658f6b80d6ca
Martín Barroso <martindanielbarroso@gmail.com>
2021-11-23 14:54:50 -0300

    1

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,9 +15,9 @@ function Contact() {
       <div className="contact-info">
         <div className="map-wrapper">
           <iframe
-            title="picardo-map"
             className="map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3350.2746988279614!2d-68.86379618424226!3d-32.890904676399835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e09a97979ea49%3A0x90b33117622cd410!2sPicardo%20-%20Bar%2C%20Picadas%20y%20Amigos!5e0!3m2!1sen!2sar!4v1637677384721!5m2!1sen!2sar"
+            title="picardo-map"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3350.2746988279614!2d-68.86379618421519!3d-32.89090467639983!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e09a97979ea49%3A0x90b33117622cd410!2sPicardo%20-%20Bar%2C%20Picadas%20y%20Amigos!5e0!3m2!1ses-419!2sar!4v1637689824769!5m2!1ses-419!2sar"
             allowfullscreen=""
             loading="lazy"
           ></iframe>
